fab2054431e2d5b033dba7dd44cd86bad65f1ffe
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-07-20 17:38:13 +0530

    Add home page to pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+const Home = () => {
+    return (
+        <>
+            <Box sx={{ flexGrow: 1, p: 2 }}>Home</Box>
+        </>
+    );
+};
+
+export default Home;
