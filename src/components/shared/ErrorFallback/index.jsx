d381c43758bdf703094b1c7c7b9f6417c3123d86
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-07-20 17:37:42 +0530

    Add error fallback component to components

---
+++ src/components/shared/ErrorFallback/index.jsx
@@ -0,0 +1,15 @@
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+    return (
+        <>
+            <div role='alert'>
+                <p>Something went wrong:</p>
+                <pre>{error.message}</pre>
+                <button type='button' onClick={resetErrorBoundary}>
+                    Try again
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default ErrorFallback;
